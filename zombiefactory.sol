558a3b03aa221209c6de0ba2ac7cec9dbfcc8e4d
Jordan <jordanonuma@outlook.com>
2018-10-23 05:57:53 -1000

    Variable representing zombie DNA will have 16 digits. Doing so by adding 'dnaModulus' that has 10^16 digits and taking its modulus 16 for the last 16 digits.

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -4,5 +4,8 @@ contract ZombieFactory {
 
   //State variable will be permamently stored on-chain
   uint dnaDigits = 16;
+  //To make sure our Zombie's DNA is only 16 characters, set 'dnaModulus' equal to 10^16.
+  //Modulus operator (%) will be used to shorten the integer to 16 digits.
+  uint dnaModulus = 10 ** dnaDigits;
 
 }
