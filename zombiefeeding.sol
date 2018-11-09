d4252ca97a52f131639444cc8627198e89be7458
Jordan <jordanonuma@outlook.com>
2018-11-09 05:50:37 -1000

    Added comment for local variable 'myZombie' that is a storage pointer to 'zombies' array.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -5,6 +5,8 @@ contract ZombieFeeding is ZombieFactory {
 
     function feedAndMultiply(uint _zombieId, uint _targetDna) public {
       require(msg.sender == zombieToOwner[_zombieId]);
+
+      //Local 'Zombie' named 'myZombie' is a storage pointer indexed '_zombieId' in the 'zombies' array.
       Zombie storage myZombie = zombies[_zombieId]; 
     } //end function feedAndMultiply()
 
