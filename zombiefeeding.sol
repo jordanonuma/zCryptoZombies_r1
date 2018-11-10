826e839072a92b566b049325b426a55522a0db9d
Jordan <jordanonuma@outlook.com>
2018-11-10 10:55:30 -1000

    Added call function _createZombie().

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -7,7 +7,11 @@ contract ZombieFeeding is ZombieFactory {
       require(msg.sender == zombieToOwner[_zombieId]);
 
       //Local 'Zombie' named 'myZombie' is a storage pointer indexed '_zombieId' in the 'zombies' array.
-      Zombie storage myZombie = zombies[_zombieId]; 
+      Zombie storage myZombie = zombies[_zombieId];
+
+      _targetDna = _targetDna % dnaModulus; //takes last 16 digits of _targetDna
+      uint newDna = (myZombie.dna + _targetDna) / 2; //access 'myZombie' properties using 'myZombie.dna' or 'myZombie.name'
+      _createZombie("NoName", newDna);
     } //end function feedAndMultiply()
 
 } //end contract ZombieFeeding {}
