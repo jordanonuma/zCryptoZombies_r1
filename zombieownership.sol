6336af5885fa63e16c46ced6a87d62c230abad64
Jordan <jordanonuma@outlook.com>
2018-12-12 05:26:35 -1000

    Added frame for ERC721 functions balanceOf() and ownerOf() that allow for user to show how many zombies they have and who owns which zombie ID, respectively.

--- zombieownership.sol
+++ zombieownership.sol
@@ -3,5 +3,11 @@ import "./zombieattack.sol";
 import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+    function balanceOf(address _owner) external view returns (uint256 _balance) {
 
+    } //end function balanceOf()
+
+    function ownerOf(uint256 _tokenId) external view returns (address _owner) {
+
+    } //end fucntion ownerOf()
 }
