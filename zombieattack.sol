2af66643f5a28d7996d8ba3a9aa3b3b5ab27046f
Jordan <jordanonuma@outlook.com>
2018-12-13 05:46:30 -1000

    Propagated ownerOf(_zombieId) -> onlyOwnerOf(_zombieId) name changes to contracts ZombieHelper {} and ZombieAttack {}.

--- zombieattack.sol
+++ zombieattack.sol
@@ -11,7 +11,7 @@ contract ZombieAttack is ZombieHelper {
     return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
   } //end function randMod()
 
-  function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+  function attack(uint _zombieId, uint _targetId) external onlyOwnerOf(_zombieId) {
     Zombie storage myZombie = zombies[_zombieId];
     Zombie storage enemyZombie = zombies[_targetId];
     uint rand = randMod(100); //function randMod(100) will output pseudorandom number between 0 and 99.
