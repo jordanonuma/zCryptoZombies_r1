e9ae6bdb194952bfc02e4f509ce3d34ff31de3d9
Jordan <jordanonuma@outlook.com>
2018-12-14 05:14:15 -1000

    Added generalized ERC721 function _transfer().

--- zombieownership.sol
+++ zombieownership.sol
@@ -10,4 +10,8 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
     function ownerOf(uint256 _tokenId) external view returns (address) {
       return zombieToOwner[_tokenId];
     } //end fucntion ownerOf()
+
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+      
+    }
 }
