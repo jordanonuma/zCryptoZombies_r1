39b50ead306e220336b8b215364bee2e81ba506a
Jordan <jordanonuma@outlook.com>
2018-12-15 07:43:02 -1000

    Added mapping of approved owners who can ask to have the zombie(s) transferred to themselves. Function _transferFrom() is able to have the original push the zombie to the new owner or have the new (approved) owner request to pull the zombie from the original owner.

--- zombieownership.sol
+++ zombieownership.sol
@@ -3,6 +3,9 @@ import "./zombieattack.sol";
 import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
+
+    mapping (uint => address) zombieApprovals;
+
     function balanceOf(address _owner) external view returns (uint256) {
       return ownerZombieCount[_owner];
     } //end function balanceOf()
@@ -16,5 +19,10 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
       ownerZombieCount[_from]--;
       zombieToOwner[_tokenId] = _to;
       emit Transfer(_from, _to, _tokenId); //dApp will send out this signal via the JSON-RPC API for the front end
+    }
+
+    function _transferFrom(address _from, address _to, uint256 _tokenId) external payable {
+      require (zombieToOwner[_tokenId] == msg.sender || zombieApprovals[_tokenId] == msg.sender);
+      _transfer(_from, _to, _tokenId);
     }
 }
