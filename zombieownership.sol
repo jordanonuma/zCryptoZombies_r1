c9ab5fea33433379bd590606b5c8b7a4fb9b0aad
Jordan <jordanonuma@outlook.com>
2018-12-17 05:49:23 -1000

    Added trigger to start log entry when approved() function is called.

--- zombieownership.sol
+++ zombieownership.sol
@@ -28,5 +28,6 @@ contract ZombieOwnership is ZombieAttack, ERC721 {
 
     function approved(address _approved, uint256 _tokenId) external payable onlyOwnerOf(_tokenId) {
       zombieApprovals[_tokenId] = _approved;
+      emit Approval(msg.sender, _approved, _tokenId);
     } //end function approved()
 } //end contract ZombieOwnership {}
