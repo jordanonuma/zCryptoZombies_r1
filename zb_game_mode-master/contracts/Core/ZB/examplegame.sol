6a5cb8b29fb119156dd4bf370bcff7d29878bc89
Jordan <jordanonuma@outlook.com>
2019-01-01 10:36:15 -1000

    Happy New Year. Declared new GameState in memory and used init() to read in data to 'gameState'.

--- zb_game_mode-master/contracts/Core/ZB/examplegame.sol
+++ zb_game_mode-master/contracts/Core/ZB/examplegame.sol
@@ -5,6 +5,8 @@ import "./ZB/ZBGameMode.sol";
 contract ExampleGame is ZBGameMode  {
 
   function beforeMatchStart(bytes serializedGameState) external {
-      
+
+    GameState memory gameState; //Declares a new GameState in memory
+    gameState.init(serializedGameState); //Function init() will read in all the data from serializedGameState and use it to populate 'gameState' with data
   } //end function beforeMatchStart()
 } //end contract ExampleGame {}
