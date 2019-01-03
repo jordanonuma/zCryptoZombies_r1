9315cc0384b6f688d5f4c1b5223e74360a588e92
Jordan <jordanonuma@outlook.com>
2019-01-03 05:32:44 -1000

    Modified player 1 and 2's defense, goo, goo vials, and max goo vials game states.

--- zb_game_mode-master/contracts/Core/ZB/examplegame.sol
+++ zb_game_mode-master/contracts/Core/ZB/examplegame.sol
@@ -12,5 +12,14 @@ contract ExampleGame is ZBGameMode  {
     //Declares custom data type 'changes'. 'Changes' will track edits made in the game state.
     ZBSerializer.SerializedGameStateChanges memory changes;
     changes.init(); //Initializes 'changes'
+
+    changes.changePlayerDefense(Player.Player1, 15);
+    changes.changePlayerDefense(Player.Player2, 15);
+    changes.changePlayerCurrentGooVials(Player.Player1, 3); //the number of vials the player currently has available for this round. The default starts at 1 when the match starts, and increases by 1 with every turn.
+    changes.changePlayerCurrentGooVials(Player.Player2, 3);
+    changes.changePlayerCurrentGoo(Player.Player1, 3); //the number of vials that are filled with goo in the current round. Defaults to be the same as the number of goo vials at the start of each turn.
+    changes.changePlayerCurrentGoo(Player.Player2, 3);
+    changes.changePlayerMaxGooVials(Player.Player1, 8); //default is 10
+    changes.changePlayerMaxGooVials(Player.Player2, 8); //default is 10
   } //end function beforeMatchStart()
 } //end contract ExampleGame {}
