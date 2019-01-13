10f42b0f73fb0a6a5f2094d7e5463d9a329546eb
Jordan <jordanonuma@outlook.com>
2019-01-13 07:39:41 -1000

    Added flags and conditional for filtering through each card, making sure it is unique.

--- zb_game_mode-master/contracts/Core/ZB/singleton.sol
+++ zb_game_mode-master/contracts/Core/ZB/singleton.sol
@@ -18,7 +18,9 @@ contract Singleton is ZBGameMode  {
             uint cardCount = 0;
 
             for (uint j = 0; j < gameState.playerStates[i].cardsInDeck.length; j++) {
-                if (isLegalCard(gameState.playerStates[i].cardsInDeck[j])) {
+                bool cardAlreadyInDeck = false;
+
+                if (!cardAlreadyInDeck) {
                     newCards[cardCount] = gameState.playerStates[i].cardsInDeck[j];
                     cardCount++;
                 } //end if (for each card)
@@ -29,7 +31,5 @@ contract Singleton is ZBGameMode  {
         changes.emit();
     } //end function beforeMatchStart()
 
-    function isLegalCard(CardInstance card) internal view returns(bool) {
-      return (card.gooCost <= 2);
-    } //end function isLegalCard()
+
 } //end contract Singleton {}
