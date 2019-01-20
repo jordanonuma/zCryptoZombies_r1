9359720f790477e09cb947c3fc9111b3bb643502
Jordan <jordanonuma@outlook.com>
2019-01-20 11:19:50 -1000

    Invoked functions name() and symbol() from ERCS721XTokenNFT.sol to give the token a name and symbol.

--- erc721x/contracts/Core/ERC721X/ZombieCard.sol
+++ erc721x/contracts/Core/ERC721X/ZombieCard.sol
@@ -2,5 +2,11 @@ pragma solidity ^0.4.25;
 import "./ERC721XToken.sol";
 
 Contract ZombieCard is ERC721XToken {
-  
-}
+  function name() external view returns (string) {
+      return "ZombieCard";
+  } //end function name()
+
+  function symbol() external view returns (string) {
+      return "ZCX";
+  } //end function symbol()
+} //end Contract ZombieCard {}
