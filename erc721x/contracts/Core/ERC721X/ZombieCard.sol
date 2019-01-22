435122fa922422b5d59efe1f054b7526b56e986b
Jordan <jordanonuma@outlook.com>
2019-01-22 05:47:46 -1000

    Added logic to the function minting ERC721x tokens to: 1) restricting minting to only once per token; 2) mint token; and 3) update the token's array with the supply.

--- erc721x/contracts/Core/ERC721X/ZombieCard.sol
+++ erc721x/contracts/Core/ERC721X/ZombieCard.sol
@@ -16,4 +16,10 @@ Contract ZombieCard is ERC721XToken {
   function tokenIndividualSupply(uint _tokenId) public view returns (uint) {
     return tokenIdToIndividualSupply[_tokenId];
   } //end function tokenIndividualSupply()
+
+  function mintToken(uint _tokenId, uint _supply) public onlyOwner {
+    require(!exists(_tokenId), "Error: Tried to mint duplicate token id"); //restricts token to being minted only once
+    _mint(_tokenId, msg.sender, _supply);
+    tokenIdToIndividualSupply[_tokenId] = _supply; //updates mapping to store the supply of the token
+  } //end function mintToken()
 } //end Contract ZombieCard {}
